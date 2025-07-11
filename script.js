94f20500a327fd1bed7d2737592ad67c22f843e2
maryam <fatimaisherewithyou@gmail.com>
2025-07-11 13:15:59 +0330

    Done ;)

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ const button= document.querySelector('button')
 const togle = document.querySelector('.underHeader');
 
 button.onclick = () => {
+    if(togle.classList.contains('hidden')) {
+        button.innerText='close menu'
+    } else {
+        button.innerText='open menu'
+    }
     togle.classList.toggle('hidden');
 }
